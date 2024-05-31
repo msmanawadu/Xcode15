c5cce7094a5462a8d98db7599f1bd7328ca683db
msmanawadu <msmanawadu@gmail.com>
2024-05-31 12:59:54 +0530

    Initial Commit

---
+++ Xcode15/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Xcode15
+//
+//  Created by Sandeepa Manawadu on 2024-05-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Xcode15/Xcode15App.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode15App.swift
+//  Xcode15
+//
+//  Created by Sandeepa Manawadu on 2024-05-31.
+//
+
+import SwiftUI
+
+@main
+struct Xcode15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
