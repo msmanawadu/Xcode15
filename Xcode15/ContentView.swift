6b5393750db7609bc84f57b9d4817f5ea7e96671
msmanawadu <msmanawadu@gmail.com>
2024-05-31 15:01:08 +0530

    Hello Text

--- Xcode15/ContentView.swift
+++ Xcode15/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Commit")
+            Text("Hello, Sandy!")
         }
         .padding()
     }
